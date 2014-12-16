792ac947e71c2fbfd937f2a857c482630a62f504
Simone Basso <bassosimone@gmail.com>
2014-12-16 19:24:31 +0100

    LuceneManager.java: codying style

--- src/main/java/it/polito/tellmefirst/lucene/LuceneManager.java
+++ src/main/java/it/polito/tellmefirst/lucene/LuceneManager.java
@@ -1,5 +1,5 @@
 /*-
- * Copyright (C) 2012 Federico Cairo, Giuseppe Futia, Federico Benedetto
+ * Copyright (C) 2012 Federico Cairo, Giuseppe Futia, Federico Benedetto.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -35,9 +35,11 @@ import java.io.IOException;
 public class LuceneManager {
 
 	static Log LOG = LogFactory.getLog(LuceneManager.class);
-	private Analyzer luceneDefaultAnalyzer = new StandardAnalyzer(Version.LUCENE_36);
+	private Analyzer luceneDefaultAnalyzer = new StandardAnalyzer(
+			Version.LUCENE_36);
 	private Directory luceneCorpusIndexDirectory;
-	// this is customized for TMF GUI
+
+	// This attribute is taylored for TMF GUI:
 	private int limitForQueryResult = 7;
 
 	public LuceneManager(Directory directory) throws IOException {
@@ -46,11 +48,12 @@ public class LuceneManager {
 		LOG.debug("[constructor] - BEGIN");
 	}
 
-	// this method is customized from DBpedia Spotlight
+	// This method is customized from DBpedia Spotlight:
 	public static Directory pickDirectory(File indexDir) throws IOException {
 		LOG.debug("[pickDirectory] - BEGIN");
 		Directory directory;
-		if (System.getProperty("os.name").equals("Linux") && System.getProperty("os.arch").contains("64")) {
+		if (System.getProperty("os.name").equals("Linux") &&
+				System.getProperty("os.arch").contains("64")) {
 			directory = new MMapDirectory(indexDir);
 		} else if (System.getProperty("os.name").equals("Linux")) {
 			directory = new NIOFSDirectory(indexDir);
@@ -80,16 +83,16 @@ public class LuceneManager {
 	public Query getQueryForContext(Text context) throws ParseException {
 		LOG.debug("[getQueryForContext] - BEGIN");
 		Query result;
-		QueryParser parser = new QueryParser(Version.LUCENE_36, "CONTEXT", this.getLuceneDefaultAnalyzer());
-		//no prod
+		QueryParser parser = new QueryParser(Version.LUCENE_36,
+			"CONTEXT", this.getLuceneDefaultAnalyzer());
 		LOG.debug("Analyzer used here: " + getLuceneDefaultAnalyzer());
-		//escape special characters
-		String queryText = context.getText().replaceAll("[\\+\\-\\|!\\(\\)\\{\\}\\[\\]\\^~\\*\\?\"\\\\:&]", " ");
+		// Escape special characters:
+		String queryText = context.getText().replaceAll(
+				"[\\+\\-\\|!\\(\\)\\{\\}\\[\\]\\^~\\*\\?\"\\\\:&]", " ");
 		queryText = QueryParser.escape(queryText);
 		result = parser.parse(queryText);
 		LOG.debug("Main query from Classify: " + result);
 		LOG.debug("[getQueryForContext] - END");
 		return result;
 	}
-
 }
